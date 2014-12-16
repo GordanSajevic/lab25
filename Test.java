1f752f319b549ffe2abf09302fc76bb052f11636
Gordan Sajević <gsajevic@hotmail.com>
2014-12-16 11:55:28 +0100

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,23 @@
+package lab25;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Enter weight: ");
+		double weight = TextIO.getDouble();
+		System.out.println("Enter width: ");
+		double width = TextIO.getDouble();
+		System.out.println("Enter length: ");
+		double length = TextIO.getDouble();
+		System.out.println("Enter height: ");
+		double height = TextIO.getDouble();
+		DHLPaket pack = new DHLPaket();
+		pack.setWeight(weight);
+		pack.setHeight(height);
+		pack.setWidth(width);
+		pack.setLength(length);
+		System.out.println("Price is: " + pack.getPrice());
+		System.out.println("Dimensional weight is: " + pack.getDimensionalWeight());
+	}
+
+}
